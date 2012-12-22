f3df774b8543c31e11232482415c65974abe9b00
Gareth Rushgrove <gareth@morethanseven.net>
2012-12-22 12:49:54 +0000

    Fix problem installing on Ubuntu 12.04
    It appears the repo has changed a little, with the non-free flag and
    trying to install the source packages I got failures in apt-get update.

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -18,11 +18,12 @@ class googlechrome::repo::debian (
   $pin
 ) {
   apt::source { $repo_name:
-    location   => $baseurl,
-    release    => $release,
-    repos      => $repos,
-    key        => $key,
-    key_source => $key_source,
-    pin        => $pin,
+    location    => $baseurl,
+    release     => $release,
+    repos       => $repos,
+    key         => $key,
+    key_source  => $key_source,
+    pin         => $pin,
+    include_src => false,
   }
 }
